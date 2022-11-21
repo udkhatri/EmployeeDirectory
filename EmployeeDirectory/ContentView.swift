f7129a579bf3887c769b2b344f62383f5c2d0835
Uday khatri <udayskhatri@gmail.com>
2022-11-21 13:05:50 -0500

    Initial Commit

---
+++ EmployeeDirectory/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EmployeeDirectory
+//
+//  Created by Uday Khatri on 2022-11-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmployeeDirectory/EmployeeDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeDirectoryApp.swift
+//  EmployeeDirectory
+//
+//  Created by Uday Khatri on 2022-11-21.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
